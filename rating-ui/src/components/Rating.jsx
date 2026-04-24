f1de2d857405cfe65b59b4bec9ed706b92319248
pedrofrohmut <pedrofrohmut@gmail.com>
2026-04-24 18:55:02 -0300

    Rating UI - use state hook

--- rating-ui/src/components/Rating.jsx
+++ rating-ui/src/components/Rating.jsx
@@ -1,4 +1,9 @@
+import { useState } from "react"
+
 const Rating = () => {
+  const [rating, setRating] = useState(0)
+  const [hover, setHover] = useState(0)
+
   const stars = [1, 2, 3, 4, 5]
 
   return (
@@ -6,7 +11,15 @@ const Rating = () => {
       <h2>Rate Your Expirence</h2>
       <div className="stars">
         {stars.map(star => (
-          <span key={star} className="star">{'\u2605'}</span>
+          <span
+            onClick={() => setRating(star)}
+            onMouseEnter={() => setHover(star)}
+            onMouseLeave={() => setHover(0)}
+            key={star}
+            className="star"
+          >
+            {'\u2605'}
+          </span>
         ))}
       </div>
     </div>
