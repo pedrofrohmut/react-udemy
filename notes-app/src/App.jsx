78cb832ecbef1a526e18df1db988973ad57d0801
pedrofrohmut <pedrofrohmut@gmail.com>
2026-04-28 14:21:42 -0300

    Notes App - NoteList

--- notes-app/src/App.jsx
+++ notes-app/src/App.jsx
@@ -1,16 +1,18 @@
 import { FaStickyNote } from 'react-icons/fa'
 import NoteForm from './components/NoteForm'
 import { useState } from 'react'
+import NoteList from './components/NoteList'
 
 const App = () => {
   const [notes, setNotes] = useState([])
 
   return (
     <div className="max-w-lg mx-auto mt-10 p-6 bg-gray-100 rounded-lg shadow-lg">
-      <h2 className="flex items-center justify-center gap-2 text-2xl font-bold mb-4 text-center">
+        <h2 className="flex items-center justify-center gap-2 text-3xl font-bold mb-4 text-center text-purple-500">
         <FaStickyNote className="relative top-[2px]"/> Notes App
-      </h2>
+        </h2>
       <NoteForm notes={notes} setNotes={setNotes} />
+      <NoteList notes={notes} />
     </div>
   )
 }
