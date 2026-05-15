5a587b502dd31ded3d74143b31828565c3f72614
pedrofrohmut <pedrofrohmut@gmail.com>
2026-05-15 17:56:02 -0300

    Fetching data for projects

--- friendly-dev/app/routes/projects/Index.tsx
+++ friendly-dev/app/routes/projects/Index.tsx
@@ -1,4 +1,17 @@
-const ProjectsPage = () => {
+import type { Project } from "~/types"
+import type { Route } from "./+types/Index"
+
+type ProjectsData = { projects: Array<Project> }
+
+export const loader = async ({ request }: Route.LoaderArgs): Promise<ProjectsData> => {
+  const response = await fetch("http://localhost:5000/projects")
+  const data = await response.json()
+  return { projects: data }
+}
+
+const ProjectsPage = ({ loaderData }: Route.ComponentProps) => {
+  const { projects } = loaderData as ProjectsData
+
   return (
     <>
       <h2 className="text-3xl font-bold text-white mb-8">Projects</h2>
