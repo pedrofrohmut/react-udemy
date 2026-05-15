416a2ebad31cb45b489f94efdd00cc71715ce008
pedrofrohmut <pedrofrohmut@gmail.com>
2026-05-15 13:47:52 -0300

    Friendly Dev - SSR & Client Hydration example

--- friendly-dev/app/routes/about/Index.tsx
+++ friendly-dev/app/routes/about/Index.tsx
@@ -1,4 +1,24 @@
+import { useEffect } from "react"
+
 const AboutPage = () => {
+  const date = new Date()
+  date.setHours(date.getHours() - 3) // To fix the time zone that goes weird when you use toISOString
+  const now = date.toISOString()
+
+  // To check out the part that happens in the client (hydration) vs the part
+  // that happens in the server (server rendering)
+  if (typeof window === "undefined") {
+    console.log("Server rendered at:", now)
+  } else {
+    console.log("Client hydration at:", now)
+  }
+
+  useEffect(() => {
+    if (window) { // In react router framework you dont need the if window
+      console.log("ScrollX: ", window.scrollX)
+    }
+  }, [])
+
   return (
     <>
       <h2 className="text-3xl font-bold text-white mb-2">About Me</h2>
