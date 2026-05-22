93c62c08db0e7844c1bc12393a91f5ad2a01ea74
pedrofrohmut <pedrofrohmut@gmail.com>
2026-05-21 21:29:57 -0300

    Friendly Dev - Filtering Posts in the BlogPage

--- friendly-dev/app/routes/blog/BlogPage.tsx
+++ friendly-dev/app/routes/blog/BlogPage.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import PostCard from "../../components/PostCard"
 import Pagination from "../../components/Pagination"
+import PostsFilter from "../../components/PostsFilter"
 
 import type { Route } from "./+types/BlogPage"
 import type { PostMeta } from "~/types"
@@ -17,14 +18,23 @@ export const loader = async ({ request }: Route.LoaderArgs): Promise<Array<PostM
 
 const BlogPage = ({ loaderData }: Route.ComponentProps) => {
   const [currentPage, setCurrentPage] = useState<number>(1)
+  const [searchQuery, setSearchQuery] = useState<string>("")
 
   const postsMeta = loaderData
 
+  const filteredPosts = postsMeta.filter((post: PostMeta) => {
+    const search = searchQuery.toLowerCase()
+    const title = post.title.toLowerCase()
+    const excerpt = post.excerpt.toLowerCase()
+
+    return title.includes(search) || excerpt.includes(search)
+  })
+
   const postsPerPage = 2
-  const totalPages = Math.ceil(postsMeta.length / postsPerPage)
+  const totalPages = Math.ceil(filteredPosts.length / postsPerPage)
 
   const first = postsPerPage * (currentPage - 1)
-  const currentPosts = postsMeta.slice(first, first + postsPerPage)
+  const currentPosts = filteredPosts.slice(first, first + postsPerPage)
 
   const handlePageChange = (n: number) => setCurrentPage(n)
 
@@ -32,10 +42,13 @@ const BlogPage = ({ loaderData }: Route.ComponentProps) => {
     <div className="max-w-3xl mx-auto mt-10 px-6 py-6">
       <h2 className="text-3xl text-white font-bold mb-8">📝 Blog</h2>
 
+      <PostsFilter searchQuery={searchQuery} onSearchChange={setSearchQuery} />
+
+      {currentPosts.length === 0 && <p className="text-gray-400 text-center">No posts found</p>}
+
       <div className="flex flex-col gap-4">
-        {currentPosts.map((postMeta: PostMeta) => (
-          <PostCard key={postMeta.slug} postMeta={postMeta} />
-        ))}
+        {currentPosts.length > 0 &&
+          currentPosts.map((postMeta: PostMeta) => <PostCard key={postMeta.slug} postMeta={postMeta} />)}
       </div>
 
       <Pagination numberOfPages={totalPages} currentPage={currentPage} handlePageChange={handlePageChange} />
