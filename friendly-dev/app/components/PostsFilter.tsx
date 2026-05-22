612130a034d5b68fb783e5fd617d48f01477eab6
pedrofrohmut <pedrofrohmut@gmail.com>
2026-05-22 10:51:57 -0300

    Friendly Dev - Using cached strings to avoid string.toLowerCase several times for each character typed

--- friendly-dev/app/components/PostsFilter.tsx
+++ friendly-dev/app/components/PostsFilter.tsx
@@ -1,15 +1,15 @@
 type PostsFilterProps = {
   searchQuery: string
-  onSearchChange: (value: string) => void
+  handleSearchChange: (value: string) => void
 }
 
-const PostsFilter: React.FC<PostsFilterProps> = ({ searchQuery, onSearchChange }) => {
+const PostsFilter: React.FC<PostsFilterProps> = ({ searchQuery, handleSearchChange }) => {
   return (
     <div className="mb-6">
       <input
         type="text"
         value={searchQuery}
-        onChange={(e) => onSearchChange(e.target.value)}
+        onChange={(e) => handleSearchChange(e.target.value)}
         placeholder="Search Posts..."
         className="w-full px-4 py-2 rounded-lg bg-gray-800 text-white border border-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
       />
--- friendly-dev/app/routes/blog/BlogPage.tsx
+++ friendly-dev/app/routes/blog/BlogPage.tsx
@@ -7,14 +7,23 @@ import { sortedPostsByDateDesc } from "../../utils"
 import type { Route } from "./+types/BlogPage"
 import type { PostMeta } from "~/types"
 
-export const loader = async ({ request }: Route.LoaderArgs): Promise<Array<PostMeta>> => {
+type CachedPostMeta = PostMeta & { lowerTitle: string; lowerExcerpt: string }
+
+export const loader = async ({ request }: Route.LoaderArgs): Promise<Array<CachedPostMeta>> => {
   const url = new URL("/posts-meta.json", request.url)
   const response = await fetch(url.href)
   if (!response.ok) {
     throw new Error("Fail to fetch data from: /posts-meta.json")
   }
   const data: Array<PostMeta> = await response.json()
-  return sortedPostsByDateDesc(data)
+  const sorted = sortedPostsByDateDesc(data)
+  const withCache = sorted.map((postMeta) =>
+    Object.assign(postMeta, {
+      lowerTitle: postMeta.title.toLowerCase(),
+      lowerExcerpt: postMeta.excerpt.toLowerCase()
+    })
+  )
+  return withCache
 }
 
 const BlogPage = ({ loaderData }: Route.ComponentProps) => {
@@ -23,13 +32,9 @@ const BlogPage = ({ loaderData }: Route.ComponentProps) => {
 
   const postsMeta = loaderData
 
-  const filteredPosts = postsMeta.filter((post: PostMeta) => {
-    const search = searchQuery.toLowerCase()
-    const title = post.title.toLowerCase()
-    const excerpt = post.excerpt.toLowerCase()
-
-    return title.includes(search) || excerpt.includes(search)
-  })
+  const filteredPosts = postsMeta.filter(
+    (post: CachedPostMeta) => post.lowerTitle.includes(searchQuery) || post.lowerExcerpt.includes(searchQuery)
+  )
 
   const postsPerPage = 2
   const totalPages = Math.ceil(filteredPosts.length / postsPerPage)
@@ -39,11 +44,15 @@ const BlogPage = ({ loaderData }: Route.ComponentProps) => {
 
   const handlePageChange = (n: number) => setCurrentPage(n)
 
+  const handleSearchChange = (query: string) => {
+    setSearchQuery(query.toLowerCase())
+  }
+
   return (
     <div className="max-w-3xl mx-auto mt-10 px-6 py-6">
       <h2 className="text-3xl text-white font-bold mb-8">📝 Blog</h2>
 
-      <PostsFilter searchQuery={searchQuery} onSearchChange={setSearchQuery} />
+      <PostsFilter searchQuery={searchQuery} handleSearchChange={handleSearchChange} />
 
       {currentPosts.length === 0 && <p className="text-gray-400 text-center">No posts found</p>}
 
