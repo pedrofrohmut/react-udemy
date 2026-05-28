9645d957739bffb1090042cd29c1ac16b55abc9d
pedrofrohmut <pedrofrohmut@gmail.com>
2026-05-28 16:30:44 -0300

    Github Finder - Check if account from token is following github user or not

--- github-finder/src/api/github.ts
+++ github-finder/src/api/github.ts
@@ -1,17 +1,50 @@
+// https://docs.github.com/en/rest/users/users?apiVersion=2026-03-10#get-a-user
 export const fetchGithubUser = async (username: string) => {
   const response = await fetch(`${import.meta.env.VITE_GITHUB_API_URL}/users/${username}`)
+
   if (!response.ok) {
     throw new Error("User not found")
   }
+
   const data = await response.json()
   return data
 }
 
+// https://docs.github.com/en/rest/search/search?apiVersion=2026-03-10#search-users
 export const searchGithubUser = async (query: string) => {
   const response = await fetch(`${import.meta.env.VITE_GITHUB_API_URL}/search/users?q=${query}&page=1&per_page=5`)
+
   if (!response.ok) {
     throw new Error("User not found")
   }
+
   const data = await response.json()
   return data
+}
+
+// https://docs.github.com/en/rest/users/followers?apiVersion=2026-03-10#check-if-a-person-is-followed-by-the-authenticated-user
+export const checkIfFollowingUser = async (username: string) => {
+  const FOLLOWING_CODE = 204
+  const NOT_FOLLOWING_CODE = 404
+
+  const response = await fetch(`${import.meta.env.VITE_GITHUB_API_URL}/user/following/${username}`, {
+    headers: {
+      Authorization: `Bearer ${import.meta.env.VITE_GITHUB_API_TOKEN}`,
+      Accept: "application/vnd.github+json",
+    }
+  })
+
+  if (!response.ok) {
+    throw new Error("Error to check if following user")
+  }
+
+  if (response.status === FOLLOWING_CODE) {
+    return true
+  }
+
+  if (response.status === NOT_FOLLOWING_CODE) {
+    return false
+  }
+
+  throw new Error("Error to check if following user")
 }
--- github-finder/src/components/UserCard.tsx
+++ github-finder/src/components/UserCard.tsx
@@ -1,19 +1,47 @@
-import { FaGithubAlt } from "react-icons/fa"
+import { useQuery } from "@tanstack/react-query"
+import { FaGithubAlt, FaUserMinus, FaUserPlus } from "react-icons/fa"
 import type { GithubUser } from "../types"
+
+import { checkIfFollowingUser } from "../api/github"
 
 type UserCardProps = {
   user: GithubUser
 }
 
 const UserCard: React.FC<UserCardProps> = ({ user }) => {
+  if (!user) {
+    return null
+  }
+
+  const { data: isFollowing, refetch } = useQuery({
+    queryKey: ["follow-status", user.login],
+    queryFn: () => checkIfFollowingUser(user.login),
+    enabled: user.login && user.login !== "",
+  })
+
   return (
     <div className="user-card">
       <img src={user.avatar_url} alt={user.name} className="avatar" />
       <h2>{user.name || user.login}</h2>
       <p className="bio">{user.bio}</p>
-      <a className="profile-btn" href={user.html_url} target="_blank" rel="noopener noreferrer">
-        <FaGithubAlt /> View Github Profile
-      </a>
+
+      <div className="user-card-buttons">
+        <button className={`follow-btn ${isFollowing ? "following" : ""}`}>
+          {isFollowing ? (
+            <>
+              <FaUserMinus className="follow-icon" /> Following
+            </>
+          ) : (
+            <>
+              <FaUserPlus className="follow-icon" /> Follow user
+            </>
+          )}
+        </button>
+
+        <a className="profile-btn" href={user.html_url} target="_blank" rel="noopener noreferrer">
+          <FaGithubAlt /> View Github Profile
+        </a>
+      </div>
     </div>
   )
 }
