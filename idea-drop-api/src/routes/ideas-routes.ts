a1ecde28419f86d7263f9f43897966f09946101e
pedrofrohmut <pedrofrohmut@gmail.com>
2026-06-09 17:12:02 -0300

    Idea Drop Api - Update and Delete Ideas; Unexpected errors forwarded to errorHandling middleware

--- idea-drop-api/src/routes/ideas-routes.ts
+++ idea-drop-api/src/routes/ideas-routes.ts
@@ -1,20 +1,41 @@
-import { Router, Request, Response } from "express"
+import { Router, Request, Response, NextFunction } from "express"
 import IdeaModel from "../models/idea-model"
+
+const getTagsArray = (tags?: string | Array<string>): { ok: boolean, tagsArray: Array<string> } => {
+  if (!tags) {
+    return { ok: true, tagsArray: [] }
+  }
+
+  if (tags && typeof tags === "string") {
+    const tagsArray = tags.split(",").map(tag => tag.trim()).filter(tag => tag !== "")
+    return { ok: true, tagsArray }
+  }
+
+  if (tags && Array.isArray(tags)) {
+    return { ok: true, tagsArray: tags }
+  }
+
+  return { ok: false, tagsArray: [] }
+}
 
 // Base url: /api/ideas
 const routeIdeas = (router: Router, baseUrl: string): void => {
+
   // public GET /api/ideas
-  router.get(baseUrl, async (req: Request, res: Response): Promise<any> => {
+  router.get(baseUrl, async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     try {
       const ideas = await IdeaModel.find()
       res.json(ideas)
     } catch (err) {
-      res.status(500).send("Unexpected error occured trying to get all ideas.")
+      // res.status(500).send("Unexpected error occured trying to get all ideas.")
+      if (err instanceof Error) {
+        next(new Error("Unexpected error occured trying to get all ideas."))
+      }
     }
   })
 
   // public GET /api/ideas/:id
-  router.get(`${baseUrl}/:id`, async (req: Request, res: Response): Promise<any> => {
+  router.get(`${baseUrl}/:id`, async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     try {
       const idea = await IdeaModel.findById(req.params.id)
       if (!idea) {
@@ -23,12 +44,15 @@ const routeIdeas = (router: Router, baseUrl: string): void => {
       }
       res.json(idea)
     } catch (err) {
-      res.status(500).send("Unexpected error occured trying to get idea by id.")
+      // res.status(500).send("Unexpected error occured trying to get idea by id.")
+      if (err instanceof Error) {
+        next(new Error("Unexpected error occured trying to get idea by id."))
+      }
     }
   })
 
   // public POST /api/ideas
-  router.post(baseUrl, async (req: Request, res: Response): Promise<void> => {
+  router.post(baseUrl, async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     const { title, summary, description, tags } = req.body
 
     if (!title.trim() || !summary.trim() || !description.trim()) {
@@ -36,14 +60,8 @@ const routeIdeas = (router: Router, baseUrl: string): void => {
       return
     }
 
-    let tagsArray: Array<string> = []
-    if (tags && typeof tags === "string") {
-      tagsArray = tags.split(",")
-                      .map(tag => tag.trim())
-                      .filter(tag => tag !== "")
-    } else if (tags && Array.isArray(tags)) {
-      tagsArray = tags.filter(tag => tag !== "")
-    } else if (tags) {
+    const { ok, tagsArray } = getTagsArray(req.body.tags)
+    if (!ok) {
       res.status(400).json({
         message: "Invalid format for tags. It can be a string with comma separated values of an array of strings"
       })
@@ -53,11 +71,74 @@ const routeIdeas = (router: Router, baseUrl: string): void => {
     try {
       const model = new IdeaModel({ title, summary, description, tags: tagsArray })
       await model.save()
+
       res.status(201).json(model)
     } catch (err) {
-      res.status(500).send("Unexpected error occured trying to save a new idea.")
+      // res.status(500).send("Unexpected error occured trying to save a new idea.")
+      if (err instanceof Error) {
+        next(new Error("Unexpected error occured trying to save a new idea: " + err.message))
+      }
     }
   })
+
+  // public PUT /api/ideas/:id
+  router.put(`${baseUrl}/:id`, async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    // const { title, summary, description, tags } = req.body
+    try {
+      const idea = await IdeaModel.findById(req.params.id)
+      if (!idea) {
+        res.status(400).json({ message: "Idea not found with the passed id. Nothing happend" })
+        return
+      }
+
+      const { ok, tagsArray } = getTagsArray(req.body.tags)
+      if (!ok) {
+        res.status(400).json({
+          message: "Invalid format for tags. It can be a string with comma separated values of an array of strings"
+        })
+        return
+      }
+
+      const updatedIdea = {
+        title: req.body.title || idea.title,
+        summary: req.body.summary || idea.summary,
+        description: req.body.description || idea.description,
+        tags: req.body.tags ? tagsArray : idea.tags
+      }
+
+      await idea.updateOne(updatedIdea)
+
+      res.status(204).send("")
+    } catch (err) {
+      if (err instanceof Error) {
+        // res.status(500).send("Unexpected error occured when trying to update an idea")
+        if (err instanceof Error) {
+          next(new Error("Unexpected error occured when trying to update an idea: " + err.message))
+        }
+      }
+    }
+  })
+
+  // public DELETE /api/ideas/:id
+  router.delete(`${baseUrl}/:id`, async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+      const idea = await IdeaModel.findById(req.params.id)
+      if (!idea) {
+        res.status(400).json({ message: "Idea not found with passed id. Nothing happend." })
+        return
+      }
+
+      await IdeaModel.deleteOne({ _id: req.params.id })
+
+      res.status(204).send("")
+    } catch (err) {
+      // res.status(500).send("Unexpected error occured when trying to delete an idea")
+      if (err instanceof Error) {
+        next(new Error("Unexpected error occured when trying to delete an idea"))
+      }
+    }
+  })
+
 }
 
 export default routeIdeas
